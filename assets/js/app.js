0b9cd1955050e053a4bbd94c99d13d87a40a8841
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2022-11-21 14:52:09 -0600

    agregado de bloque,cantidad y añadir a carrito

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,45 +20,63 @@ const Hamburguesa_clásica = new Hamburguesa(1,"Hamburguesa Clásica","Medallón
 const Hamburguesa_Hawaiana = new Hamburguesa(2,"Hamburguesa Hawaiana", "Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla Piña y Chile.", 65,"tarjeta2","assets/img/hawaiana.png","#ffab91")
 const Hamburguesa_Especial = new Hamburguesa(3,"Hamburguesa Doble", "Doble Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla y Chile.",  80,"tarjeta3","assets/img/doble.png","#81d4fa")
  
-
 //query 
 
 const main2 = document.querySelector("#main2")
 const imgbloque =document.querySelector("#imgbloquedos")
 const titulo = document.querySelector("#title")
 const desc = document.querySelector("#desc")
-const precio = document.querySelector(".pos5")
+const precio = document.querySelector("#presio")
 const colordef =document.querySelector("#colordef")
+
+const cantidad =document.querySelector("#IntroduceCantidad")
+const preciobloque = document.querySelector(".pos5")
+const botonañadir = document.querySelector("#botonañadir")
 
 //Array de objetos
 const Hamburguesas = [Hamburguesa_clásica,Hamburguesa_Hawaiana,Hamburguesa_Especial]
+
+const añadiendo_productos =[]; 
 
 
 // Craccion de elementos en el dom (bloque1) con foreach
 
 //funciones 
 
-const describir = (e)=> {
+const rederizardatos = (e)=> {
   const hamselecta = e.target.closest(".llamado").getAttribute("data-id");
-
+  
   const hamelegida = Hamburguesas.find((Ham) => Ham.id ==hamselecta)
-  console.log(hamelegida);
-
+  // console.log(hamelegida);
+  
   titulo.textContent = hamelegida.nombre
   desc.textContent = hamelegida.ingrediente
   precio.textContent = `$${hamelegida.precio}`
   imgbloque.setAttribute("src",hamelegida.imagen)
- 
+  botonañadir.setAttribute("data-id",hamelegida.id)
 
-  console.log(hamelegida.nombre);
-
-
+    
+  botonañadir.addEventListener("click",agregararray)
+  
 }
+const agregararray =  (e)=>{
+  
+  const hamselecta = e.target.getAttribute("data-id")
+  const hamelegida =Hamburguesas.find((Ham)=>Ham.id ==hamselecta)
+  
+  añadiendo_productos.push(hamelegida.precio*cantidad.value)
+  console.log(añadiendo_productos);
+  
+  const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0);
+console.log(total);
+}
+
+
 
 //función que renderiza en el DOM,los objetos de un array 
 
 const renderizaobjetos = () => {
-  
+  añadiendo_productos.innerHTML=""
   Hamburguesas.forEach((Ham) => {
     let div = document.createElement("div")
     div.className = `col-lg-12 col-md-12 col-ms-12 ${Ham.color} rounded-4 altura` ;
@@ -68,30 +86,30 @@ const renderizaobjetos = () => {
     <img class="rounded-circle bg-white p-1 posicono" src= ${Ham.imagen} alt="" srcset="" width="50" height="50">
     <h1 class="tituloicono">${Ham.nombre}</h1> 
     </div > `
-    
+       
     main2.appendChild(div)
   });
-
-
+   
   const divs = document.querySelectorAll(".llamado")
   divs.forEach((div) => {
-  div.addEventListener ('click', describir)
+    div.addEventListener ('click', rederizardatos)
   })
-  
-
-
-
-
-
-
-
+    
 }
+
+
+
+
+
+//Eventlisteners
 
 
 
 
 //ejecución
 renderizaobjetos();
+
+
  
 
 
--- assets/js/appd.js
+++ assets/js/appd.js
@@ -1,101 +1,116 @@
 
-let app = document.getElementById("app");
-app.addEventListener("click", reinicio)
+// let app = document.getElementById("app");
+// app.addEventListener("click", reinicio)
 
 
-let cantidad=""
+// // let cantidad=""
+// const añadiendo_productos =[]; 
+
+// function AñadirCarro() {
+
+//   añadiendo_productos.push (hamelegida.nombre*cantidad)
+//   console.log("añadiendo_productos");
 
 
-function reinicio() {
+// }
+// let añadirBoton = document.getElementById("añadirBoton");
+// app.addEventListener("click", AñadirCarro)
 
-  const añadiendo_productos =[];  
+
+
+
+// AñadirCarro
+
+// function reinicio() {
+
+//   const añadiendo_productos =[];  
     
-  function solicitar() {
-do {
-   cantidad = prompt("ingrese cantidad")
-} while (cantidad == "" || cantidad == 0 );
+//   function solicitar() {
+// do {
+//    cantidad = cantidad.value
+// } while (cantidad == "" || cantidad == 0 );
 
-  }  
+//   }  
 
-  function pedido() {
+//   function pedido() {
     
-    opción=prompt("Ingrese el producto que desea  acorde al numero correspondiente:\n1. Hamburguesa clásica  \n2. Hamburguesa Hawaiana \n3. Hamburguesa doble");
+//     opción=prompt("Ingrese el producto que desea  acorde al numero correspondiente:\n1. Hamburguesa clásica  \n2. Hamburguesa Hawaiana \n3. Hamburguesa doble");
       
-    opción=Number(opción);
+//     opción=Number(opción);
   
-  switch (opción) {
+//   switch (opción) {
   
-    case 1:
-      precio=Hamburguesa_clásica.precio;
-      solicitar()
-      añadiendo_productos.push (Hamburguesa_clásica.precio*cantidad)
-      console.log(añadiendo_productos);
-      break;
+//     case 1:
+//       precio=Hamburguesa_clásica.precio;
+//       solicitar()
+//       añadiendo_productos.push (Hamburguesa_clásica.precio*cantidad)
+//       console.log(añadiendo_productos);
+//       break;
   
-    case 2:
-      precio=Hamburguesa_Hawaiana.precio;
-      solicitar()
-      añadiendo_productos.push (Hamburguesa_Hawaiana.precio*cantidad);
-      console.log(añadiendo_productos);
-      break;
+//     case 2:
+//       precio=Hamburguesa_Hawaiana.precio;
+//       solicitar()
+//       añadiendo_productos.push (Hamburguesa_Hawaiana.precio*cantidad);
+//       console.log(añadiendo_productos);
+//       break;
   
-    case 3:
-      precio=Hamburguesa_Especial.precio;
-      solicitar()
-      añadiendo_productos.push (Hamburguesa_Especial.precio*cantidad);
-      console.log(añadiendo_productos);
-      break;
+//     case 3:
+//       precio=Hamburguesa_Especial.precio;
+//       solicitar()
+//       añadiendo_productos.push (Hamburguesa_Especial.precio*cantidad);
+//       console.log(añadiendo_productos);
+//       break;
   
-    default:
-        alert("No solicito Nada");
-        pedido()
-        break;
+//     default:
+//         alert("No solicito Nada");
+//         pedido()
+//         break;
       
     
-  }
+//   }
   
-  opción2=prompt( "¿Desea agregar algo mas a su cuenta: \n1. si , Seguir Comprando  \n2. No, La cuenta por Favor ");
+//   opción2=prompt( "¿Desea agregar algo mas a su cuenta: \n1. si , Seguir Comprando  \n2. No, La cuenta por Favor ");
   
-  if (opción2==2) {
+//   if (opción2==2) {
     
-    Opción3= prompt("Solicita Servicio a Domicilio:\n1. si  \n2. No ");
+//     Opción3= prompt("Solicita Servicio a Domicilio:\n1. si  \n2. No ");
     
-    if (Opción3==1) {
-      let cliente=prompt("Ingrese su Nombre por favor");
-      let dirección=prompt("Ingrese su dirección");
+//     if (Opción3==1) {
+//       let cliente=prompt("Ingrese su Nombre por favor");
+//       let dirección=prompt("Ingrese su dirección");
       
-      const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0);
-      console.log(total);
-      totalmasiva = total+15;
+//       const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0);
+//       console.log(total);
+//       totalmasiva = total+15;
       
-      alert("Estimado(a) "+ cliente +" Su cuenta es de "+ totalmasiva + " y se entrega en la siguiente dirección: " + dirección );
+//       alert("Estimado(a) "+ cliente +" Su cuenta es de "+ totalmasiva + " y se entrega en la siguiente dirección: " + dirección );
       
       
-    }
-    if (Opción3==2) {
-      let cliente=prompt("Ingrese su Nombre por favor");
-      const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0);
-      console.log(total);
-      totalmasiva = total;
-      alert("Estimado(a) "+ cliente +" Su cuenta es de "+ totalmasiva + " y se entrega en sucursal" );
+//     }
+//     if (Opción3==2) {
+//       let cliente=prompt("Ingrese su Nombre por favor");
+//       const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0);
+//       console.log(total);
+//       totalmasiva = total;
+//       alert("Estimado(a) "+ cliente +" Su cuenta es de "+ totalmasiva + " y se entrega en sucursal" );
          
-    }
+//     }
     
-    if (opción2 == "" || opción2 == 0 || opción2 !=1 || opción2 !=2 ) {
-      alert("corregir")
-    }
+//     if (opción2 == "" || opción2 == 0 || opción2 !=1 || opción2 !=2 ) {
+//       alert("corregir")
+//     }
 
       
-  }
+//   }
   
-  if (opción2 == 1) {
-      pedido()
-  }
+//   if (opción2 == 1) {
+//       pedido()
+//   }
   
-  }
+//   }
   
-  pedido();
-  }
+//   pedido();
+//   }
 
 
 
